015a1bbbfa94342a561781f6c5eb05e8fd4642c2
Epulapp <Epulapp@EPULAPP72.univ-lyon1.fr>
2016-05-17 10:53:12 +0200

    Creation Case

--- TP_Demineur_MVC/src/tp_demineur_mvc/Modeles/Case.java
+++ TP_Demineur_MVC/src/tp_demineur_mvc/Modeles/Case.java
@@ -5,10 +5,47 @@
  */
 package tp_demineur_mvc.Modeles;
 
+import java.util.ArrayList;
+
 /**
  *
  * @author Epulapp
  */
 public class Case {
-    private String mdr;
+    private boolean visite;
+    private int nombreMinesAutour;
+    private boolean mine;
+    private Plateau plateau;
+
+    public Case(Plateau p) {
+        visite = false;
+        plateau = p;
+    }
+
+    public boolean getVisite() {
+        return visite;
+    }
+
+    public int getNombreMinesAutour() {
+        return nombreMinesAutour;
+    }
+
+    public boolean isMine() {
+        return mine;
+    }
+    
+    
+    public void majClick(){
+        if(!visite && nombreMinesAutour ==0){
+            ArrayList<Case> casesVoisines = plateau.getVoisins();
+            for(Case c : casesVoisines){
+                c.visite = true;
+                c.majClick();
+            }
+        }
+    }
+    
+    
+    
+    
 }
