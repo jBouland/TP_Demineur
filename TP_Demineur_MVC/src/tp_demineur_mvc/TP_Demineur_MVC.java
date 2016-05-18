b5cebc5c7f50c3a5239bf7de8fdefc0682ef76c0
Epulapp <Epulapp@EPULAPP72.univ-lyon1.fr>
2016-05-18 16:08:18 +0200

    Creation Interface graphique

--- TP_Demineur_MVC/src/tp_demineur_mvc/TP_Demineur_MVC.java
+++ TP_Demineur_MVC/src/tp_demineur_mvc/TP_Demineur_MVC.java
@@ -7,38 +7,73 @@ package tp_demineur_mvc;
 
 import javafx.application.Application;
 import javafx.event.ActionEvent;
+import javafx.event.Event;
 import javafx.event.EventHandler;
+import javafx.event.EventType;
+import javafx.geometry.Insets;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.input.MouseButton;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.ColumnConstraints;
+import javafx.scene.layout.GridPane;
+import javafx.scene.layout.HBox;
 import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
+import tp_demineur_mvc.Modeles.Plateau2D;
 
 /**
  *
  * @author Epulapp
  */
 public class TP_Demineur_MVC extends Application {
-    
+
     @Override
-    public void start(Stage primaryStage) {
-        Button btn = new Button();
-        btn.setText("Say 'Hello World'");
-        btn.setOnAction(new EventHandler<ActionEvent>() {
-            
-            @Override
-            public void handle(ActionEvent event) {
-                System.out.println("Hello World!");
+    public void start(Stage stage) {
+
+        Plateau2D board = new Plateau2D();
+
+        BorderPane border = new BorderPane();
+        GridPane grid = new GridPane();
+//        grid.setHgap(10);
+//        grid.setVgap(10);
+        ImageView[][] listCases = new ImageView[10][10];
+
+        HBox hbox = new HBox();
+        border.setTop(hbox);
+        border.setCenter(grid);
+
+        ImageView selectedImage = new ImageView();
+        final Image caseClose = new Image("Assets/CaseClose.png");
+        final Image CaseFlag = new Image("Assets/CaseFlag.png");
+        selectedImage.setImage(caseClose);
+
+        for (int i = 0; i < 10; i++) {
+            for (int j = 0; j < 10; j++) {
+                listCases[i][j] = new ImageView();
+                listCases[i][j].setImage(caseClose);
+                grid.add(listCases[i][j], i, j);
+                listCases[i][j].setOnMouseClicked(new EventHandler<MouseEvent>() {
+                    @Override
+                    public void handle(MouseEvent t) {
+                        if(t.getButton() == MouseButton.SECONDARY){
+                            ImageView ImageSource = (ImageView)t.getSource();
+                            ImageSource.setImage(CaseFlag);
+                        }
+                    }
+                });
             }
-        });
-        
-        StackPane root = new StackPane();
-        root.getChildren().add(btn);
-        
-        Scene scene = new Scene(root, 300, 250);
-        
-        primaryStage.setTitle("Hello World!");
-        primaryStage.setScene(scene);
-        primaryStage.show();
+        }
+
+        Scene scene = new Scene(border, 600, 600);
+        // root.getChildren().addAll(selectedImage);
+        scene.setRoot(border);
+        stage.setTitle("Démineur");
+        stage.setScene(scene);
+        stage.show();
     }
 
     /**
@@ -47,5 +82,5 @@ public class TP_Demineur_MVC extends Application {
     public static void main(String[] args) {
         launch(args);
     }
-    
+
 }
